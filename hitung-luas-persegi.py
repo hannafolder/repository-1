3ba9aaa0743c2a7115e6c31c21592373577dd779
hannafolder <abcdefghannaa@gmail.com>
2026-03-25 20:05:27 +0700

    Delete hitung-luas-persegi.py

--- hitung-luas-persegi.py
+++
@@ -1,8 +0,0 @@
-print("Selamat datang, kamu bisa menghitung Luas Persegi di sini")
-
-sisi_str = input("Silahkan masukan jumlah sisi dalam cm: ")
-
-sisi_cm = float(sisi_str)
-
-luasPersegi = sisi_cm ** 2
-print("Jumlah luas persegi adalah: ", luasPersegi)
